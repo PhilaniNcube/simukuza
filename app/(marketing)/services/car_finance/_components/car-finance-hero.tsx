b9bae4632f86aebbf8ed3b67a28341d1ede18672
Philani Ncube <ncbphi001@gmail.com>
2025-01-06 10:51:13 +0200

    Refactor Car Finance Hero layout and add FinanceContent component

--- app/(marketing)/services/car_finance/_components/car-finance-hero.tsx
+++ app/(marketing)/services/car_finance/_components/car-finance-hero.tsx
@@ -3,22 +3,26 @@ import Image from "next/image";
 const CarFinanceHero = () => {
   return (
     <div className="relative">
-      <Image
-        src="https://utfs.io/f/84aXfFFbF7G0TyOBkmtdZe0PC5Gk8TyQmWExKaMgw3Jl9D4H"
-        alt="Car Finance Hero"
-        className="object-cover w-full h-[50vh] md:h-[70vh] lg:h-[80vh]"
-        width={1920}
-        height={1080}
-      />
-      <div className="absolute inset-0 bg-darkgrey bg-opacity-50">
-        <div className="mx-auto max-w-7xl px-4 h-full flex items-center justify-center">
-          <div className="relative flex flex-col items-center justify-center h-full text-center text-white">
-            <h1 className="text-4xl font-light leading-tight md:text-5xl lg:text-6xl">
-              Car Finance
-            </h1>
-            <p className="mt-4 text-lg md:text-xl font-bold lg:text-2xl">
-              Find the best car finance deals for you
-            </p>
+      <div className="max-w-7xl mx-auto pt-32 pb-12 px-4 @container">
+        <div className="grid grid-cols-1 @md:grid-cols-2 @lg:grid-cols-3 gap-6">
+          <Image
+            src="https://utfs.io/f/84aXfFFbF7G0TyOBkmtdZe0PC5Gk8TyQmWExKaMgw3Jl9D4H"
+            alt="Car Finance Hero"
+            className="object-cover w-full h-[50vh] @md:h-[70vh] @lg:h-[80vh] col-span-1 @lg:col-span-2"
+            width={1920}
+            height={1080}
+          />
+          <div className="col-span-1">
+            <div className="h-full flex items-center">
+              <div className="relative flex flex-col h-full justify-center text-accent">
+                <h1 className="text-4xl font-light leading-tight md:text-5xl lg:text-6xl">
+                  Car Finance
+                </h1>
+                <p className="mt-4 text-lg md:text-xl text-darkblue lg:text-2xl">
+                  Find the best car finance deals for you
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
---
+++ app/(marketing)/services/car_finance/_components/finance-content.tsx
@@ -0,0 +1,6 @@
+const FinanceContent = () => {
+  return <section className="max-w-7xl mx-auto px-4 pb-12">
+
+  </section>;
+};
+export default FinanceContent;
